c6839e6314fcf3ac1bc101c0e0955593499f9687
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-12-01 18:26:03 +0100

    Update parts-led.ino

--- code/parts-led.ino
+++ code/parts-led.ino
@@ -20,14 +20,56 @@
  ** LED:
  *** Longer leg:  Anode  (+)
  *** Shorter leg: Catode (-)
+ *** LED is ON  = HIGH (1)
+ *** LED IS OFF = LOW  (0)
  */
 
 
+#define PIN_LED 4 /* GPIO <2;13> pin connected to LED (+) AKA Longer leg */
+
+
+unsigned char led_state = LOW; /* start LED in turned off STATE */
+
+
 void setup() {
-  
+  /* initialize serial communication at 9600 bits per second */
+  Serial.begin(9600);
+
+  /* initialize the LED (+) pin as OUTPUT */
+  pinMode(PIN_LED, OUTPUT);
+
+  /* start LED in turned off STATE */
+  digitalWrite(PIN_LED, led_state);
+}
+
+
+/* Turns the LED On */
+void turnLedOn() {
+  led_state = HIGH;
+  digitalWrite(PIN_LED, led_state);
+}
+
+
+/* Turns the LED Off */
+void turnLedOff() {
+  led_state = LOW;
+  digitalWrite(PIN_LED, led_state);
+}
+
+
+/* Toggle LED to opposite state */
+void turnLedToggle() {
+  if(led_state) {
+    led_state = LOW;
+  }
+  else {
+    led_state = HIGH;
+  }
+  digitalWrite(PIN_LED, led_state);
 }
 
 
 void loop() {
-
+  //turnLedToggle();
+  //delay(1500);
 }
