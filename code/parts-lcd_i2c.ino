347d729b884cfea3590b90853012fc8974272d49
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-12-01 15:56:15 +0100

    Create parts-lcd_i2c.ino

---
+++ code/parts-lcd_i2c.ino
@@ -0,0 +1,20 @@
+/* Arduino IDE Set up:
+ ** Tools -> Port -> /dev/ttyACM0
+ ** Tools -> Board -> Arduino Uno
+ */
+
+/* Docs:
+ ** Arduino Uno:
+ *** https://store.arduino.cc/products/arduino-uno-rev3/
+ ** LCD I2C:
+ *** https://arduinogetstarted.com/tutorials/arduino-lcd-i2c
+ */
+
+/* Schematic:
+ ** LCD I2C:
+ *** Arduino UNO GROUND <=> LCD I2C GND
+ *** Arduino UNO 5V     <=> LCD I2C VCC
+ *** Arduino UNO A<0;5> <=> LCD I2C SDA 
+ *** Arduino UNO A<0;5> <=> LCD I2C SCL
+ */
+
