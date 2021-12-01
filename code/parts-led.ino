9af93a276070a7aa798a05cd8b29fbf903772283
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-12-01 18:09:04 +0100

    Create parts-led.ino

---
+++ code/parts-led.ino
@@ -0,0 +1,33 @@
+/* Arduino IDE Set up:
+ ** Tools -> Port -> /dev/ttyACM0
+ ** Tools -> Board -> Arduino Uno
+ */
+
+/* Docs:
+ ** Arduino Uno:
+ *** https://store.arduino.cc/products/arduino-uno-rev3/
+ ** LED:
+ *** https://roboticsbackend.com/arduino-led-complete-tutorial/
+ */
+
+/* Schematic:
+ ** LED:
+ *** Arduino UNO GROUND       <=> LED (-) AKA Shoreter leg
+ *** Arduino UNO GPIO D<2;13> <=> Resistor 220 Ohm <=> LED (+) AKA Longer leg 
+ */
+
+/* Theory:
+ ** LED:
+ *** Longer leg:  Anode  (+)
+ *** Shorter leg: Catode (-)
+ */
+
+
+void setup() {
+  
+}
+
+
+void loop() {
+
+}
