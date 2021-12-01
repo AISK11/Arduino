e8385c20554873f88040b5220292bc796ea4fc6d
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-12-01 12:06:34 +0100

    Create arduino_uno-relay.c

---
+++ code/arduino_uno-relay.c
@@ -0,0 +1,59 @@
+/************************************************
+ * Author: AISK11                               *
+ * Description: code to toggle (ON/OFF) RELAY.  *
+ * Date Created: 2021-12-01                     *
+ * Last Updated: 2021-12-01                     *
+ ************************************************/
+/* Arduino IDE Set up:
+ ** Tools -> Port -> /dev/ttyACM0
+ ** Tools -> Board -> Arduino Uno
+ */
+
+/* Schematic:
+ ** Arduino UNO GPIO D<2;13> <=> Relay
+ ** GROUND                   <=> Relay
+ */
+
+/* Docs:
+ ** Arduino Uno:
+ *** https://store.arduino.cc/products/arduino-uno-rev3/
+ ** Button:
+ *** https://arduinogetstarted.com/tutorials/arduino-button
+ */ 
+
+/* Theory:
+ ** Relay is OFF = 1 (HIGH)
+ ** Relay is ON = 0 (LOW)
+ */
+
+
+#define RELAY_PIN 3  /* GPIO <2;13> pin connected to Relay INPUT */
+
+
+void setup() {
+  /* initialize serial communication at 9600 bits per second */
+  Serial.begin(9600);
+
+  /* initialize the Relay input pin as OUTPUT */
+  pinMode(RELAY_PIN, OUTPUT);
+  
+  /* Start relay in off state (OFF = HIGH) */
+  relayTurnOff();
+}
+
+
+void relayTurnOff() {
+  Serial.println("Relay is OFF.");
+  digitalWrite(RELAY_PIN, HIGH);
+}
+
+
+void relayTurnOn() {
+  Serial.println("Relay is ON.");
+  digitalWrite(RELAY_PIN, LOW);
+}
+
+
+void loop() {
+  //Placeholder
+}
