b36316219c87330af528a6dbae33ef0eabef4d74
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-12-01 16:36:41 +0100

    Update parts-lcd_i2c.ino

--- code/parts-lcd_i2c.ino
+++ code/parts-lcd_i2c.ino
@@ -8,6 +8,7 @@
  *** https://store.arduino.cc/products/arduino-uno-rev3/
  ** LCD I2C:
  *** https://arduinogetstarted.com/tutorials/arduino-lcd-i2c
+ *** https://arduinogetstarted.com/reference/library/arduino-lcd-library
  */
 
 /* Schematic:
@@ -29,18 +30,41 @@
 LiquidCrystal_I2C lcd(0x27, 20, 4); /* I2C address 0x27, 20 column and 4 rows */
 
 
-
 void setup() {
   lcd.init();      /* initialize the lcd */
   lcd.backlight(); /* open the backlight */
 
+  lcd.noCursor();  /* hide LCD cursor */
+}
+
+
+/* Function does the following:
+ * 1. Prints "Hello World" followed with "-" for whole second line.
+ * 2. After 1 second, clears the screen.
+ * 3. After 1 second, prints "Bye World" followed with "-" for whole second line.
+ * 4. After 1 second, clears the screen and repeats step 1.
+ */
+void lcdMessage() {
   lcd.setCursor(0, 0);                 /* move cursor   to (0, 0) */
   lcd.print("Hello World!");           /* print message at (0, 0) */
   lcd.setCursor(0, 1);                 /* move cursor   to (0, 1) */
   lcd.print("--------------------");   /* print message at (0, 0) */
+
+  delay(1000);
+  lcd.clear();
+  delay(1000);
+
+  lcd.setCursor(0, 0);                 /* move cursor   to (0, 0) */
+  lcd.print("Bye World!");           /* print message at (0, 0) */
+  lcd.setCursor(0, 1);                 /* move cursor   to (0, 1) */
+  lcd.print("--------------------");   /* print message at (0, 0) */  
+
+  delay(1000);
+  lcd.clear();
+  delay(1000);
 }
 
 
 void loop() {
-  //Placeholder
+  lcdMessage();
 }
