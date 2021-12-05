73b191d311bdf0ebc94be53ed2a23a279c8897e2
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-12-05 22:22:37 +0100

    Update parts-potentiometer.ino

--- code/parts-potentiometer.ino
+++ code/parts-potentiometer.ino
@@ -2,7 +2,7 @@
  * Author: AISK11                               *
  * Description: code to control potentiometer.  *
  * Date Created: 2021-12-01                     *
- * Last Updated: 2021-12-01                     *
+ * Last Updated: 2021-12-05                     *
  ************************************************/
 /* Arduino IDE Set up:
  ** Tools -> Port -> /dev/ttyACM0
@@ -30,31 +30,13 @@
  */
 
 
-#define PIN_POTENTIOMETER A1 /* GPIO <A0;A5> pin connected to LM35 OUTPUT */
+/* GPIO <A0;A5> pin connected to LM35 OUTPUT */
+#define PIN_POTENTIOMETER A1
 
 
 void setup() {
-  /* initialize serial communication at 9600 bits per second */
+  /* Initialize serial communication at 9600 bits per second. */
   Serial.begin(9600);
-}
-
-
-unsigned short int getPotentiometerOutput() {
-  /* read value from potentiometer, range: 0-1023 */
-  unsigned short int potentiometer_analog = analogRead(PIN_POTENTIOMETER);
-
-  return potentiometer_analog;
-}
-
-
-unsigned short int getPotentiometerOutputRemapped(unsigned short int remap_min, unsigned short int remap_max) {
-  /* read value from potentiometer, range: 0-1023 */
-  unsigned short int potentiometer_analog = analogRead(PIN_POTENTIOMETER);
-
-  /* mathematically remap potentiometer from range 0-1023 to 0,100 */
-  unsigned short int potentiometer_remapped = map(potentiometer_analog, 0, 1023, remap_min, remap_max);
-
-  return potentiometer_remapped;
 }
 
 
@@ -66,4 +48,23 @@ void loop() {
   Serial.print(getPotentiometerOutputRemapped(0, 100));
   Serial.print("%\n");
   delay(1000);
+}
+
+
+unsigned short int getPotentiometerOutput() {
+  /* Read value from potentiometer, range: 0-1023. */
+  unsigned short int potentiometer_analog = analogRead(PIN_POTENTIOMETER);
+
+  return potentiometer_analog;
+}
+
+
+unsigned short int getPotentiometerOutputRemapped(unsigned short int remap_min, unsigned short int remap_max) {
+  /* Read value from potentiometer, range: 0-1023. */
+  unsigned short int potentiometer_analog = analogRead(PIN_POTENTIOMETER);
+
+  /* Mathematically remap potentiometer from range 0-1023 to 0,100. */
+  unsigned short int potentiometer_remapped = map(potentiometer_analog, 0, 1023, remap_min, remap_max);
+
+  return potentiometer_remapped;
 }
