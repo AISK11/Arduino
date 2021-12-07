a8b5d65ca184f6643275db35c474ed01fe6abf08
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-12-07 03:22:15 +0100

    Update esp32.ino

--- code/ESP32/esp32.ino
+++ code/ESP32/esp32.ino
@@ -17,26 +17,86 @@
  *** Press "EN" button.
  */
 
-/* Schematic:
+/* Schematic:/* Arduino IDE Set up:
+ ** Tools -> Port -> /dev/ttyUSB0
+ ** File -> Preferences -> Additional Boards Manager URLs:
+ *** https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json -> OK
+ ** Tools -> Board -> Boards Manager -> Arduino AVR Boards -> Install|Update
+ ** Tools -> Board -> Boards Manager -> esp32 -> Install
+ ** Tools -> Board -> ESP32 Arduino -> ESP32 Dev Module
+ */
+
+/* ERROR FIX:
+ ** "python": executable file not found in $PATH
+ *** > sudo ln -s /usr/bin/python3 /usr/bin/python
+ ** ModuleNotFoundError: No module named ’serial’:
+ *** > sudo python3 -m pip install pyserial
+ ** Output is "GARBAGE":
+ *** Baud: 9600
+ *** Press "EN" button.
+ */
+
+/* Docs:
+ ** Arduino ESP32:
+ *** https://randomnerdtutorials.com/esp32-pinout-reference-gpios/
+ *** https://www.mischianti.org/2020/05/30/esp32-pinout-specs-and-arduino-ide-configuration-part-1/
+ ** Button:
+ *** https://arduinogetstarted.com/tutorials/arduino-button
+ */ 
+
+/* Schematic (ESP32 DevKitC V4):
+ *          +---------------+
+ *      3.3V|o            ~o|GND-----+
+ *        EN|o            ~o|GPIO23  |
+ *    GPIO36|o            ~o|GPIO22  |
+ *    GPIO39|o            ~o|GPIO1   |
+ *    GPIO34|o            ~o|GPIO3   |
+ *    GPIO35|o            ~o|GPIO21  |
+ *    GPIO32|o~           ~o|GND-----+
+ *    GPIO33|o~           ~o|GPIO19
+ *    GPIO25|o~           ~o|GPIO18
+ *    GPIO26|o~           ~o|GPIO5
+ *    GPIO27|o~           ~o|GPIO17
+ *    GPIO14|o~           ~o|GPIO16
+ *    GPIO12|o~           ~o|GPIO4
+ *       GND|o            ~o|GPIO0
+ *    GPIO13|o~           ~o|GPIO2
+ *    GPIO09|o~           ~o|GPIO15
+ *    GPIO10|o~           ~o|GPIO7
+ *    GPIO11|o~           ~o|GPIO8
+ *        5V|o  EN   Boot ~o|GPIO6
+ *          +------USB------+
+ */
+
+
+void setup() {
+  /* Initialize serial communication at 9600 bits per second. */
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  Serial.println("TEST");
+}
  *          +-------------+
  *      3.3V|o           o|GND---+
- *        EN|o           o|IO23  |
- * SENSOR VP|o           o|IO22  |
- * SENSOR VN|o           o|TXD0  |
- *      IO34|o           o|RXD0  |
- *      IO35|o           o|IO21  |
- *      IO32|o           o|GND---+
- *      IO33|o           o|IO19
- *      IO25|o           o|IO18
- *      IO26|o           o|IO5
- *      IO27|o           o|IO17
- *      IO14|o           o|IO16
- *      IO12|o           o|IO4
- *       GND|o           o|IO0
- *      IO13|o           o|IO2
- *       SD2|o           o|IO15
- *       SD3|o           o|SD1
- *       CMD|o           o|SD0
+ *        EN|o--+        o|IO23  |
+ * SENSOR VP|o  |        o|IO22  |
+ * SENSOR VN|o  |        o|TXD0  |
+ *      IO34|o  |        o|RXD0  |
+ *      IO35|o  |        o|IO21  |
+ *      IO32|o  |        o|GND---+
+ *      IO33|o  |        o|IO19
+ *      IO25|o  |        o|IO18
+ *      IO26|o  |        o|IO5
+ *      IO27|o  |        o|IO17
+ *      IO14|o  |        o|IO16
+ *      IO12|o  |        o|IO4
+ *       GND|o  |        o|IO0
+ *      IO13|o  |        o|IO2
+ *       SD2|o  |        o|IO15
+ *       SD3|o  |        o|SD1
+ *       CMD|o  |        o|SD0
  *        5V|o EN   Boot o|CLK
  *          +-----USB-----+
  */
