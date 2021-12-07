044dd9130a3fb748335964a5d9da63f4ba3f467f
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-12-07 03:32:09 +0100

    Update esp32.ino

--- code/ESP32/esp32.ino
+++ code/ESP32/esp32.ino
@@ -17,25 +17,6 @@
  *** Press "EN" button.
  */
 
-/* Schematic:/* Arduino IDE Set up:
- ** Tools -> Port -> /dev/ttyUSB0
- ** File -> Preferences -> Additional Boards Manager URLs:
- *** https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json -> OK
- ** Tools -> Board -> Boards Manager -> Arduino AVR Boards -> Install|Update
- ** Tools -> Board -> Boards Manager -> esp32 -> Install
- ** Tools -> Board -> ESP32 Arduino -> ESP32 Dev Module
- */
-
-/* ERROR FIX:
- ** "python": executable file not found in $PATH
- *** > sudo ln -s /usr/bin/python3 /usr/bin/python
- ** ModuleNotFoundError: No module named ’serial’:
- *** > sudo python3 -m pip install pyserial
- ** Output is "GARBAGE":
- *** Baud: 9600
- *** Press "EN" button.
- */
-
 /* Docs:
  ** Arduino ESP32:
  *** https://randomnerdtutorials.com/esp32-pinout-reference-gpios/
@@ -66,39 +47,6 @@
  *    GPIO11|o~           ~o|GPIO8
  *        5V|o  EN   Boot ~o|GPIO6
  *          +------USB------+
- */
-
-
-void setup() {
-  /* Initialize serial communication at 9600 bits per second. */
-  Serial.begin(9600);
-}
-
-
-void loop() {
-  Serial.println("TEST");
-}
- *          +-------------+
- *      3.3V|o           o|GND---+
- *        EN|o--+        o|IO23  |
- * SENSOR VP|o  |        o|IO22  |
- * SENSOR VN|o  |        o|TXD0  |
- *      IO34|o  |        o|RXD0  |
- *      IO35|o  |        o|IO21  |
- *      IO32|o  |        o|GND---+
- *      IO33|o  |        o|IO19
- *      IO25|o  |        o|IO18
- *      IO26|o  |        o|IO5
- *      IO27|o  |        o|IO17
- *      IO14|o  |        o|IO16
- *      IO12|o  |        o|IO4
- *       GND|o  |        o|IO0
- *      IO13|o  |        o|IO2
- *       SD2|o  |        o|IO15
- *       SD3|o  |        o|SD1
- *       CMD|o  |        o|SD0
- *        5V|o EN   Boot o|CLK
- *          +-----USB-----+
  */
 
 
