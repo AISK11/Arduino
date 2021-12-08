1aeba4bc766a8b2e125fb2b19372f3c628a50ea8
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-12-08 19:34:29 +0100

    Update parts-potentiometer.ino

--- code/parts-potentiometer.ino
+++ code/parts-potentiometer.ino
@@ -2,7 +2,7 @@
  * Author: AISK11                               *
  * Description: code to control potentiometer.  *
  * Date Created: 2021-12-01                     *
- * Last Updated: 2021-12-05                     *
+ * Last Updated: 2021-12-08                     *
  ************************************************/
 /* Arduino IDE Set up:
  ** Tools -> Port -> /dev/ttyACM0
@@ -30,10 +30,16 @@
  */
 
 
+/************************
+ **     GPIO Pins      ** 
+ ************************/
 /* GPIO <A0;A5> pin connected to LM35 OUTPUT */
-#define PIN_POTENTIOMETER A1
+static const unsigned char PIN_POTENTIOMETER = A1;
 
 
+/************************
+ ** Arduino Functions  ** 
+ ************************/
 void setup() {
   /* Initialize serial communication at 9600 bits per second. */
   Serial.begin(9600);
@@ -42,29 +48,36 @@ void setup() {
 
 void loop() {
   Serial.print("Potentiometer value: ");
-  Serial.print(getPotentiometerOutput());
+  Serial.print(getPotentiometerOutput(PIN_POTENTIOMETER));
   Serial.print(";\n");
   Serial.print("Potentiometer percentage: ");
-  Serial.print(getPotentiometerOutputRemapped(0, 100));
+  Serial.print(getPotentiometerOutputRemapped(PIN_POTENTIOMETER, 0, 100));
   Serial.print("%\n");
   delay(1000);
 }
 
 
-unsigned short int getPotentiometerOutput() {
+/************************
+ ** Program Functions  ** 
+ ************************/
+/* Returns value <0-1023> equal to potentiometer position. */
+unsigned short int getPotentiometerOutput(const unsigned char potentiometer_pin) {
   /* Read value from potentiometer, range: 0-1023. */
-  unsigned short int potentiometer_analog = analogRead(PIN_POTENTIOMETER);
+  unsigned short int potentiometer_analog = analogRead(potentiometer_pin);
 
+  /* Returns value <0-1023> equal to potentiometer position. */
   return potentiometer_analog;
 }
 
 
-unsigned short int getPotentiometerOutputRemapped(unsigned short int remap_min, unsigned short int remap_max) {
+/* Returns value <remap_min-remap_max> equal to potentiometer position. */
+unsigned short int getPotentiometerOutputRemapped(const unsigned char potentiometer_pin, unsigned short int remap_min, unsigned short int remap_max) {
   /* Read value from potentiometer, range: 0-1023. */
-  unsigned short int potentiometer_analog = analogRead(PIN_POTENTIOMETER);
+  unsigned short int potentiometer_analog = analogRead(potentiometer_pin);
 
   /* Mathematically remap potentiometer from range 0-1023 to 0,100. */
   unsigned short int potentiometer_remapped = map(potentiometer_analog, 0, 1023, remap_min, remap_max);
 
+  /* Returns value <remap_min-remap_max> equal to potentiometer position. */
   return potentiometer_remapped;
 }
