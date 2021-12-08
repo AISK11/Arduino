29ac2f924d0a2e622427d2a44dc8945985bc0d17
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-12-08 19:40:43 +0100

    Update parts-relay.ino

--- code/parts-relay.ino
+++ code/parts-relay.ino
@@ -2,7 +2,7 @@
  * Author: AISK11                               *
  * Description: code to toggle (ON/OFF) RELAY.  *
  * Date Created: 2021-12-01                     *
- * Last Updated: 2021-12-05                     *
+ * Last Updated: 2021-12-08                     *
  ************************************************/
 /* Arduino IDE Set up:
  ** Tools -> Port -> /dev/ttyACM0
@@ -30,10 +30,16 @@
  */
 
 
+/************************
+ **     GPIO Pins      ** 
+ ************************/
 /* GPIO <2;13> pin connected to Relay INPUT. */
-#define PIN_RELAY 3
+static const unsigned char PIN_RELAY = 3;
 
 
+/************************
+ ** Arduino Functions  ** 
+ ************************/
 void setup() {
   /* Initialize serial communication at 9600 bits per second. */
   Serial.begin(9600);
@@ -42,21 +48,25 @@ void setup() {
   pinMode(PIN_RELAY, OUTPUT);
   
   /* Start relay in off state (OFF = HIGH). */
-  relayTurnOff();
+  relayTurnOff(PIN_RELAY);
 }
 
 
 void loop() {
-  //Placeholder
+  //relayTurnOff(PIN_RELAY);
+  //relayTurnOn(PIN_RELAY);
 }
 
 
+/************************
+ ** Program Functions  ** 
+ ************************/
 /* Turn the Relay OFF. */
-void relayTurnOff() {
+void relayTurnOff(const unsigned char pin_relay) {
   /* Sets: OFF = 1 (HIGH). */
-  digitalWrite(PIN_RELAY, HIGH);
+  digitalWrite(pin_relay, HIGH);
   /* Reads 1 (OFF). */
-  unsigned char relay_state = digitalRead(PIN_RELAY);
+  unsigned char relay_state = digitalRead(pin_relay);
   
   Serial.print("Relay is in state: ");
   Serial.print(relay_state);
@@ -65,11 +75,11 @@ void relayTurnOff() {
 
 
 /* Turn the Relay ON. */
-void relayTurnOn() {
+void relayTurnOn(const unsigned char pin_relay) {
   /* Sets: ON = 0 (LOW). */
-  digitalWrite(PIN_RELAY, LOW);
+  digitalWrite(pin_relay, LOW);
   /* Reads 0 (ON). */
-  unsigned char relay_state = digitalRead(PIN_RELAY);
+  unsigned char relay_state = digitalRead(pin_relay);
   
   Serial.print("Relay is in state: ");
   Serial.print(relay_state);
