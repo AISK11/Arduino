08e530462b774dda391fe07c1dcfe2a4580dc309
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-12-08 03:52:08 +0100

    Update func-smartDelay.ino

--- code/func-smartDelay.ino
+++ code/func-smartDelay.ino
@@ -3,7 +3,7 @@
  * Description: function to provide delay, but  *
  *              other code can still run.       *
  * Date Created: 2021-12-01                     *
- * Last Updated: 2021-12-05                     *
+ * Last Updated: 2021-12-08                     *
  ************************************************/
 /* Arduino IDE Set up:
  ** Tools -> Port -> /dev/ttyACM0
@@ -11,12 +11,18 @@
  */
 
 
+/************************
+ **  Public Variables  ** 
+ ************************/
 /* When program starts, 0 milliseconds passed already (cause it starts now). */
 unsigned long _last_millis_count = 0;
 /* Timer for smartDelay() in milliseconds. */
 unsigned long timer_without_delay = 1000;
 
 
+/************************
+ ** Arduino Functions  ** 
+ ************************/
 void setup() {
   /* Initialize serial communication at 9600 bits per second. */
   Serial.begin(9600);
@@ -30,6 +36,9 @@ void loop() {
 }
 
 
+/************************
+ ** Program Functions  ** 
+ ************************/
 /* smartDelay()
  ** Check if timer has passed + other code can still run unlike delay().
  * millis()
